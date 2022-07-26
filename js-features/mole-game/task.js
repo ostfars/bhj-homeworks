39c3868f0d3729a068396a97567d0568dd6c5a81
ostfars <v_minin@rambler.ru>
2022-07-26 23:18:49 +0300

    task solved

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,36 @@
+const dead = document.getElementById("dead");
+const lost = document.getElementById("lost");
+
+getHole = index => document.getElementById(`hole${index}`);
+
+let hitCounter = +dead.textContent;
+let missCounter = +lost.textContent;
+
+for (i = 1; i < document.getElementsByClassName("hole").length + 1; i++) {
+  const hole = getHole(i);
+  hole.onclick = clickCount = () => {
+    if (hole.className.includes( 'hole_has-mole' )) {
+      hitCounter++;
+      dead.textContent = hitCounter;
+      } else {
+      missCounter++;
+      lost.textContent = missCounter;
+      }
+    if (hitCounter === 10) {
+      alert('Победа!')
+      hitCounter = 0;
+      missCounter = 0;
+      dead.textContent = hitCounter;
+      lost.textContent = missCounter;
+      }
+    if (missCounter === 5) {
+      alert('Вы проиграли!')
+      hitCounter = 0;
+      missCounter = 0;
+      dead.textContent = hitCounter;
+      lost.textContent = missCounter;
+      }
+    dead.textContent = hitCounter;
+    lost.textContent = missCounter;
+    }
+};
