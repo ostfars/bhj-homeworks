78f6faea6fd4295ceb0688451b84eddf8696c085
ostfars <v_minin@rambler.ru>
2022-07-26 23:12:19 +0300

    bug fixed

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -3,19 +3,10 @@ const timer = document.getElementById("timer");
 timeLeft = +timer.textContent
 
 const countdownTimer = setInterval(function() {
+  timeLeft -= 1;
+  timer.textContent = timeLeft;
   if (timeLeft <= 0) {
     alert('Вы победили в конкурсе!');
     clearInterval(countdownTimer);
   };
-  timeLeft -= 1;
-  timer.textContent = timeLeft;
-  
 }, 1000)
-
-// function countDown() {
-//   let timeLeft = +initialTimer.textContent;
-//   if (timeLeft = 0) {
-//     alert('Вы победили в конкурсе!');
-//   }
-
-// }
