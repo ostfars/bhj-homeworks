69d05c66e0210e10af05620a0655fdcaa43feafa
ostfars <v_minin@rambler.ru>
2022-07-30 15:09:25 +0300

    task 3 advanced solved

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,22 +1,14 @@
-const sliderItems = document.getElementsByClassName("slider__item");
-const sliderArray = Array.from(sliderItems);
+const sliderArray = Array.from(document.getElementsByClassName("slider__item"));
+const dotsArray = Array.from(document.getElementsByClassName("slider__dot"));
+
+console.log(dotsArray);
 
 let currentImageIndex, nextImageIndex, prevImageIndex;
 
 function getImageIndexes() {
   currentImageIndex = sliderArray.indexOf(sliderArray.find(item => item.className.includes("slider__item_active")))
-
-  if (currentImageIndex === sliderArray.length - 1) {
-    nextImageIndex = 0;
-  } else {
-    nextImageIndex = currentImageIndex + 1;
-  }
-  
-  if (currentImageIndex === 0) {
-    prevImageIndex = sliderArray.length - 1;
-  } else {
-    prevImageIndex = currentImageIndex - 1;
-  };
+  nextImageIndex = currentImageIndex === sliderArray.length - 1 ? 0 : currentImageIndex + 1;
+  prevImageIndex = currentImageIndex === 0 ? prevImageIndex = sliderArray.length - 1 : prevImageIndex = currentImageIndex - 1;
 }
 
 function switchToNext() {
@@ -31,7 +23,24 @@ function switchToPrev() {
   sliderArray[prevImageIndex].classList.add("slider__item_active");
 }
 
+// function switchToDot(index) {
+//   getImageIndexes();
+//   sliderArray[currentImageIndex].classList.remove("slider__item_active");
+//   sliderArray[index].classList.add("slider__item_active");
+// }
+
 const nextArrow = document.getElementsByClassName("slider__arrow_next")[0];
 const prevArrow = document.getElementsByClassName("slider__arrow_prev")[0];
 nextArrow.onclick = switchToNext;
 prevArrow.onclick = switchToPrev;
+
+for (i = 0; i < dotsArray.length; i++) {
+  const index = i;
+  dotsArray[i].onclick = () => {
+    getImageIndexes();
+    sliderArray[currentImageIndex].classList.remove("slider__item_active");
+    sliderArray[index].classList.add("slider__item_active");
+    console.log(currentImageIndex)
+  };
+}
+console.log(dotsArray);
