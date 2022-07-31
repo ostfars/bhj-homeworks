cc8e5b0ac424b1d8b758d83c3ab086bb106d06fa
ostfars <v_minin@rambler.ru>
2022-07-31 15:48:47 +0300

    task 1 solved

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,24 +1,22 @@
-const dropdownValue = document.querySelector(".dropdown__value");
-const dropdownList = document.querySelector(".dropdown__list");
-const valuesList = Array.from(document.getElementsByClassName("dropdown__link"));
+const dropdownMenusList = Array.from(document.querySelectorAll(".dropdown"));
+const dropdownLinks = Array.from(document.getElementsByClassName("dropdown__link"))
 
-for (i=0; i < valuesList.length; i++) {
-  valuesList[i].onclick = setButtonValue;
+for (i = 0; i < dropdownMenusList.length; i++) {
+  dropdownMenusList[i].querySelector(".dropdown__value").onclick = setDropdownListActive;  
 }
 
+for (i=0; i < dropdownLinks.length; i++) {
+  dropdownLinks[i].onclick = setButtonValue;
+  }
+
 function setDropdownListActive() {
-  dropdownList.classList.add("dropdown__list_active");
+  this.closest(".dropdown").querySelector(".dropdown__list").classList.add("dropdown__list_active");
+  //  Так тоже работает:
+  // this.nextElementSibling.classList.add("dropdown__list_active");
 }
 
 function setButtonValue() {
-  dropdownValue.textContent = this.textContent;
-  dropdownList.classList.remove("dropdown__list_active");
-  return false
-}
-
-dropdownValue.addEventListener('click', setDropdownListActive)
-
-
-
-console.log(dropdownValue);
-console.log(valuesList);
+    this.closest(".dropdown").querySelector(".dropdown__value").textContent = this.textContent;
+    this.closest(".dropdown").querySelector(".dropdown__list").classList.remove("dropdown__list_active");
+    return false
+  }
