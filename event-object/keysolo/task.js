77053ebb4d03057a015bca3498860aeec7a922cb
ostfars <v_minin@rambler.ru>
2022-08-01 14:14:42 +0300

    task 2 basic solution

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,13 +17,10 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-     */
+    console.log(this.currentSymbol.textContent)
+    document.addEventListener('keyup', event => {
+      event.key === this.currentSymbol.textContent ? this.success() : this.fail()
+    })
   }
 
   success() {
