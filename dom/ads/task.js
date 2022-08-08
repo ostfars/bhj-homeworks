bc5334269ac7a406e5bc59f1545f8824b31c3e49
ostfars <v_minin@rambler.ru>
2022-08-08 11:03:38 +0300

    updated

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -12,9 +12,10 @@ console.log(rotatorsCases)
 setInterval(() => {
   for (i = 0; i < rotatorsCases.length; i++) {
     const activeCaseIndex = rotatorsCases[i].indexOf(rotatorsCases[i].find(item => item.className.includes("rotator__case_active")))
-    const nextCaseIndex = activeCaseIndex === rotatorsCases[i].length - 1 ? 0 : activeCaseIndex + 1;
-    console.log(activeCaseIndex, nextCaseIndex)
-  
+    // const nextCaseIndex = activeCaseIndex === rotatorsCases[i].length - 1 ? 0 : activeCaseIndex + 1;
+    
+    const nextCaseIndex = (activeCaseIndex + 1) % rotatorsCases[i].length;
+
     rotatorsCases[i][activeCaseIndex].classList.remove("rotator__case_active")
     rotatorsCases[i][nextCaseIndex].classList.add("rotator__case_active")
   }
